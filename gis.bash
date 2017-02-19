0c6ceed0b144409599134df0638cb146713730de
Robert Byrne <robert@byrnemail.org>
2017-02-19 12:33:23 +0000

    Install QGIS

---
+++ gis.bash
@@ -0,0 +1,4 @@
+brew tap homebrew/science
+inst_brew matplotlib
+sudo ln -s /System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python /Library/Python/2.7/site-packages/matplotlib-override
+inst_cask qgis
