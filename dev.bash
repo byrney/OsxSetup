c1204bdae9058591a3493143a56c8704d5e72615
Robert Byrne <robert@byrnemail.org>
2017-05-14 00:16:11 +0100

    Update completions to new brew locations

--- dev.bash
+++ dev.bash
@@ -1,8 +1,8 @@
 
 inst_brew ruby
-inst_brew homebrew/completions/ruby-completion
-inst_brew homebrew/completions/rake-completion
-inst_brew homebrew/completions/gem-completion
+inst_brew ruby-completion
+inst_brew rake-completion
+inst_brew gem-completion
 inst_brew git-sh
 inst_brew node
 inst_brew reattach-to-user-namespace
@@ -12,13 +12,13 @@ inst_brew wget
 inst_brew nmap
 inst_brew xmlstarlet
 inst_brew colordiff
-inst_brew subversion
+#inst_brew subversion
 inst_brew trash
 inst_brew shellcheck
 inst_brew cmake
 inst_cask dash
 inst_cask dashlane
-inst_cask iphoney
+#inst_cask iphoney
 inst_gem bundler
 inst_gem terminal-notifier
 inst_cask kdiff3
--- ops.bash
+++ ops.bash
@@ -3,7 +3,7 @@ inst_cask cord
 inst_cask virtualbox
 inst_cask virtualbox-extension-pack
 inst_cask vagrant
-inst_brew homebrew/completions/vagrant-completion
+inst_brew vagrant-completion
 inst_cask chefdk
 inst_cask xquartz
 inst_cask docker
