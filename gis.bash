819f0645ce6c16a97cb7a1b4eb9fd28ac490e261
Robert Byrne <robert@byrnemail.org>
2017-05-14 00:16:41 +0100

    Idempotent matplitlib

--- gis.bash
+++ gis.bash
@@ -1,4 +1,6 @@
 brew tap homebrew/science
 inst_brew matplotlib
-sudo ln -s /System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python /Library/Python/2.7/site-packages/matplotlib-override
+if [[ ! -L "/Library/Python/2.7/site-packages/matplotlib-override" ]] ; then
+    sudo ln -s /System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python /Library/Python/2.7/site-packages/matplotlib-override
+fi
 inst_cask qgis
