c83bef0f5eee804654bf0f547bd2129173bfa700
Robert Byrne <robert@byrnemail.org>
2017-10-24 23:01:09 +0100

    Add ripgrep

--- base.bash
+++ base.bash
@@ -2,7 +2,7 @@
 #
 # Must haves brew
 #
-for brew in "bash-completion" git tmux the_silver_searcher vim fzf ; do
+for brew in "bash-completion" git tmux the_silver_searcher ripgrep vim fzf ; do
     inst_brew "$brew"
 done
 
