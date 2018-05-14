567eccd5eeae1a7acb8e0ad75c842182e7d76aa6
Robert Byrne <robert@byrnemail.org>
2018-05-14 23:24:01 +0100

    Replace ag with rg

--- base.bash
+++ base.bash
@@ -2,7 +2,7 @@
 #
 # Must haves brew
 #
-for brew in "bash-completion" git tmux the_silver_searcher ripgrep vim fzf ; do
+for brew in "bash-completion" git tmux ripgrep vim fzf ; do
     inst_brew "$brew"
 done
 
