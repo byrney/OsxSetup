e3239670aa9b09ffeebc6ffe76aff73078eb161f
Robert Byrne <robert@byrnemail.org>
2018-05-14 23:24:49 +0100

    Remove some unused packages

--- dev.bash
+++ dev.bash
@@ -2,7 +2,6 @@
 inst_brew ruby
 inst_brew ruby-completion
 inst_brew rake-completion
-inst_brew gem-completion
 inst_brew git-sh
 inst_brew node
 inst_brew yarn
@@ -12,15 +11,11 @@ inst_brew terminal-notifier
 inst_brew wget
 inst_brew nmap
 inst_brew xmlstarlet
-inst_brew colordiff
 #inst_brew subversion
 inst_brew trash
 inst_brew shellcheck
 inst_brew cmake
 inst_cask dash
 inst_cask dashlane
-#inst_cask iphoney
 inst_gem bundler
-#inst_gem terminal-notifier
-inst_cask kdiff3
 
